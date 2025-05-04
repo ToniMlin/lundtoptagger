bdef3ffe4e0fdf67b809815e6b9ac77e9d14ecc5
Toni Mlinarevic <toni.mlinarevic.20@ucl.ac.uk>
2025-05-04 21:24:07 +0200

    Config for selecting Z jets

--- make_pT_hist.py
+++ make_pT_hist.py
@@ -23,8 +23,8 @@ def main():
     infile = args.infile
     outfile = args.outfile
 
-    path_to_files = infile if infile is not None else '/eos/home-t/tmlinare/Lund/jetetmiss/JETMDataMC/jpierre/run/submitDir-2025-01-06-1052-fee0 files 1-50/data-ANALYSIS/mc20_13TeV.802017.Py8EG_A14NNPDF23LO_WprimeWZ_flatpT_wideWmass.deriv.DAOD_JETM2.e8482_s3797_r13145_p5548.root'
-    # path_to_files = infile if infile is not None else '/eos/home-t/tmlinare/Lund/jetetmiss/JETMDataMC/jpierre/run/submitDir-2025-01-12-1919-bc4c W files 1-50 (not flat mass)/data-ANALYSIS/mc20_13TeV.801859.Py8EG_A14NNPDF23LO_WprimeWZ_flatpT.deriv.DAOD_JETM2.e8482_s3681_r13145_p5548.root'
+    # path_to_files = infile if infile is not None else '/eos/home-t/tmlinare/Lund/jetetmiss/JETMDataMC/jpierre/run/submitDir-2025-01-06-1052-fee0 files 1-50/data-ANALYSIS/mc20_13TeV.802017.Py8EG_A14NNPDF23LO_WprimeWZ_flatpT_wideWmass.deriv.DAOD_JETM2.e8482_s3797_r13145_p5548.root'
+    path_to_files = infile if infile is not None else '/eos/home-t/tmlinare/Lund/jetetmiss/JETMDataMC/jpierre/run/submitDir-2025-01-12-1919-bc4c W files 1-50 (not flat mass)/data-ANALYSIS/mc20_13TeV.801859.Py8EG_A14NNPDF23LO_WprimeWZ_flatpT.deriv.DAOD_JETM2.e8482_s3681_r13145_p5548.root'
     files = glob.glob(path_to_files)
 
     intreename = "AnalysisTree"
@@ -39,7 +39,7 @@ def main():
             tree = infile[intreename]
 
             truth_labels = ak.flatten(tree["LRJ_truthLabel"].array(library="ak"))
-            jet_pts = ak.flatten(tree["LRJ_pt"].array(library="ak"))[truth_labels==2]  # but sample also includes some QCD jets which are included in training
+            jet_pts = ak.flatten(tree["LRJ_pt"].array(library="ak"))[truth_labels==5]  # but sample also includes some QCD jets which are included in training
     
     # Create histogram
     hist = TH1F("pt", "Jet pT Histogram", 100, 100, 3000)
@@ -49,8 +49,9 @@ def main():
         hist.Fill(pt)
 
     # Save histogram to a ROOT file
-    outfile_path = outfile if outfile is not None else "pTs/Wwidemass.root"
+    # outfile_path = outfile if outfile is not None else "pTs/Wwidemass.root"
     # outfile_path = outfile if outfile is not None else "pTs/Wrealmass.root"
+    outfile_path = outfile if outfile is not None else "pTs/Z.root"
     output_file = TFile(outfile_path, "RECREATE")
     hist.Write()
     output_file.Close()
